480456cf59b32d25e1b3cc796aba9904a99a7c4b
Hugo Biarge <hugo@arosbi.com>
2020-03-19 18:28:52 +0100

    Update to .Net Core 3.1

--- src/Authorization-Workshop/src/Products.Api.Host/Startup.cs
+++ src/Authorization-Workshop/src/Products.Api.Host/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Products.Api.Host
 {
@@ -36,7 +37,7 @@ namespace Products.Api.Host
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
